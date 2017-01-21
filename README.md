916fe92a1159fd87638983593d3ee461baa1b176
Felix <felix@plesoianu.ro>
2017-01-21 11:32:22 +0200

    Upload early alpha.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# lunar-logo
-Toy scripting language based on Logo and Lua
+Lunar Logo
+==========
+
+
+Welcome to Lunar Logo, a toy scripting language based on Logo and Lua, with a tiny core and very clean syntax, currently implemented as a stand-alone Python module. Example usage:
+
+	$ ./lunar.py sqrt add mul 3 3 mul 4 4
+	5.0
+
+That's right, you can type Lunar Logo code at a Bash prompt without escaping it (within reason). For a bigger sample, put this code in a file, say `benchmark1.lulz`:
+
+	-- Loop-and-math benchmark
+	make start timer
+	make a 1
+	for i 1 1000 1 do
+		make a add div :a 2 div :a 3
+	end
+	print :a
+	make finish timer
+	print sub :finish :start
+
+Now you can load it as follows:
+
+	$ ./lunar.py load benchmark1.lulz
+	6.588005489477243e-80
+	0.21355808000000004
+
+Yes, it's slow. You're not going to write real time games in Lunar Logo. Still reasonably fast for something implemented in under 300 lines of Python. Speaking of which.
+
+Project goals
+-------------
+
+- A language that needs little or no escaping when embedded into string literals, command lines and such.
+- A language that doesn't run *too* slowly when implemented in another interpreted language.
+- A language that throws few exceptions. A surprising amount of modern languages in widespread use have other error handling mechanisms.
+
+History
+-------
+
+This is the second time I do a Logo dialect. [The first time around][ll] I kept much closer to the original language, but the result was a messy implementation that left much of the heavy lifting to individual procedures, and still didn't have much in the way of speed or capabilities.
+
+[ll]: http://felixplesoianu.github.io/little-logo/
+
+Status
+------
+
+As of 21 January 2017, Lunar Logo supports 50 procedures (you can find the list at the end of `lunar.py`). The language is, however, not very robust. Here's what happens if you misspell a procedure name:
+
+	$ ./lunar.py sqr add mul 3 3 mul 4 4
+	25
+
+You don't even get an indication of what's wrong. But you *might* get a crash, or very strange output. So this needs to be fixed before going forward.
---
+++ lunar.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+
+import math
+import random
+import time
+
+class Scope:
+	def __init__(self, parent = None):
+		self.names = {}
+		self.parent = parent
+	
+	def __getitem__(self, key):
+		#key = key.lower()
+		if key in self.names:
+			return self.names[key]
+		elif self.parent != None:
+			return self.parent[key]
+		else:
+			raise KeyError("Undefined variable: " + str(key))
+	
+	def __setitem__(self, key, value):
+		key = key.lower()
+		if key in self.names:
+			self.names[key] = value
+		elif self.parent != None:
+			self.parent[key] = value
+		else:
+			#raise KeyError("Undefined variable: " + str(key))
+			self.names[key] = value
+
+def parse(words):
+	code = []
+	buf = None
+	in_list = False
+	for i in words:
+		if in_list == True:
+			if i.endswith("]"):
+				if len(i) > 1:
+					buf.append(i[:-1])
+				code.append(buf)
+				in_list = False
+			else:
+				buf.append(i)
+		elif i == "[]":
+			code.append([])
+		elif i.startswith("["):
+			if i.endswith("]"):
+				code.append([i[1:-1]])
+			elif len(i) > 1:
+				buf = []
+				buf.append(i[1:])
+				in_list = True
+		elif i.startswith("--"):
+			break
+		elif i.startswith(":"):
+			code.append(i.lower())
+		elif i.lower() in ["do", "end"]:
+			code.append(i.lower())
+		elif i.lower() == "true":
+			code.append(True)
+		elif i.lower() == "false":
+			code.append(False)
+		elif i in procedures:
+			code.append(procedures[i])
+		elif i.isdigit():
+			code.append(int(i))
+		else:
+			try:
+				code.append(float(i))
+			except ValueError:
+				code.append(i)
+	if in_list:
+		raise SyntaxError("Unclosed list at end of line.")
+	else:
+		return code
+
+def eval_next(code, cursor, scope):
+	value = code[cursor]
+	if type(value) == tuple:
+		cursor += 1
+		args = []
+		for i in range(value[0]):
+			if cursor >= len(code):
+				raise SyntaxError(
+					"Unexpected end of input in eval.")
+			tmp, cursor = eval_next(code, cursor, scope)
+			args.append(tmp)
+		return value[1](scope, *args), cursor
+	elif type(value) != str:
+		return value, cursor + 1
+	elif value[0] == ":":
+		name = value[1:]
+		return scope[name], cursor + 1
+	elif value == "do":
+		return scan_block(code, cursor + 1)
+	else:
+		return value, cursor + 1
+
+def scan_block(code, cursor):
+	block = []
+	while code[cursor] != "end":
+		if code[cursor] == "do":
+			tmp, cursor = scan_block(code, cursor + 1)
+			block.append(tmp)
+		else:
+			block.append(code[cursor])
+			cursor += 1
+		if cursor >= len(code):
+			raise SyntaxError(
+				"Unexpected end of input in block.")
+	return block, cursor + 1
+
+
+def run(code, scope):
+	cursor = 0
+	while cursor < len(code):
+		value, cursor = eval_next(code, cursor, scope)
+	return value
+
+def load(filename, scope):
+	code = []
+	with open(filename, "r") as f:
+		for i in f:
+			code.extend(parse(i.split()))
+	return run(code, scope)
+
+
+def do_print(value):
+	if type(value) == list:
+		return print(" ".join(value))
+	else:
+		return print(value)
+
+def do_type(value):
+	if type(value) == list:
+		return print(" ".join(value), end='')
+	else:
+		return print(value, end='')
+
+
+def make(varname, value, scope):
+	scope[varname] = value
+	return value
+
+def local(varname, scope):
+	if type(varname) == list:
+		for i in varname:
+			scope.names[i.lower()] = None
+	else:
+		scope.names[varname.lower()] = None
+
+
+def do_if(cond, code, scope):
+	if cond:
+		return run(code, scope)
+	else:
+		return None
+
+def do_ifelse(cond, ift, iff, scope):
+	if cond:
+		return run(parse(ift), scope)
+	else:
+		return run(parse(iff), scope)
+
+def do_test(cond, scope):
+	if cond:
+		scope.test = True
+
+def do_iftrue(code, scope):
+	if scope.test:
+		return run(code, scope)
+	else:
+		return None
+
+def do_iffalse(code, scope):
+	if not scope.test:
+		return run(code, scope)
+	else:
+		return None
+
+def do_while(cond, code, scope):
+	cond = parse(cond)
+	value = None
+	while run(cond, scope):
+		value = run(code, scope)
+	return value
+
+def do_for(varname, init, limit, step, code, scope):
+	make(varname, init, scope)
+	value = None
+	if limit >= init:
+		while scope[varname] <= limit:
+			value = run(code, scope)
+			scope[varname] += step
+	else:
+		while scope[varname] >= limit:
+			value = run(code, scope)
+			scope[varname] += step
+	return value
+
+def do_foreach(varname, items, code, scope):
+	value = None
+	for i in items:
+		make(varname, i, scope)
+		value = run(code, scope)
+	return value
+
+def iseq(init, limit):
+	if init <= limit:
+		return list(range(init, limit + 1))
+	else:
+		return list(range(init, limit - 1, -1))
+
+procedures = {
+	"parse": (1, lambda scope, code: parse(code)),
+	"run": (1, lambda scope, code: run(code, scope)),
+	"load": (1, lambda scope, f: load(f, scope)),
+
+	"print": (1, lambda scope, item: do_print(item)),
+	"type": (1, lambda scope, item: do_type(item)),
+	"show": (1, lambda scope, item: print(item)),
+	
+	"readline": (0, lambda scope: input().split()),
+	"readword": (0, lambda scope: input()),
+
+	"make": (2, lambda scope, a, b: make(a, b, scope)),
+	"name": (2, lambda scope, a, b: make(b, a, scope)),
+	"local": (1, lambda scope, n: local(n, scope)),
+	
+	"if": (2, lambda scope, a, b: do_if(a, b, scope)),
+	"ifelse": (3, lambda scope, a, b, c: do_ifelse(a, b, c, scope)),
+	"test": (1, lambda scope, code: do_test(code, scope)),
+	"iftrue": (1, lambda scope, code: do_iftrue(code, scope)),
+	"iffalse": (1, lambda scope, code: do_iffalse(code, scope)),
+	
+	"while": (2, lambda scope, a, b: do_while(a, b, scope)),
+	"for": (5, lambda scope, v, f, t, s, c: do_for(v, f, t, s, c, scope)),
+	"foreach": (3, lambda scope, v, i, c: do_foreach(v, i, c, scope)),
+	
+	"add": (2, lambda scope, a, b: a + b),
+	"sub": (2, lambda scope, a, b: a - b),
+	"mul": (2, lambda scope, a, b: a * b),
+	"div": (2, lambda scope, a, b: a / b),
+	"mod": (2, lambda scope, a, b: a % b),
+	"pow": (2, lambda scope, a, b: a ** b),
+	"minus": (1, lambda scope, n: - n),
+	"abs": (1, lambda scope, n: abs(n)),
+	"int": (1, lambda scope, n: int(n)),
+	
+	"sqrt": (1, lambda scope, n: math.sqrt(n)),
+	"sin": (1, lambda scope, n: math.sin(n)),
+	"cos": (1, lambda scope, n: math.cos(n)),
+	
+	"lte": (2, lambda scope, a, b: a <= b),
+	"lt": (2, lambda scope, a, b: a < b),
+	"eq": (2, lambda scope, a, b: a == b),
+	"neq": (2, lambda scope, a, b: a != b),
+	"gt": (2, lambda scope, a, b: a > b),
+	"gte": (2, lambda scope, a, b: a >= b),
+	
+	"and": (2, lambda scope, a, b: a and b),
+	"or": (2, lambda scope, a, b: a or b),
+	"not": (1, lambda scope, cond: not cond),
+	
+	"first": (1, lambda scope, seq: seq[0]),
+	"last": (1, lambda scope, seq: seq[-1]),
+	"butfirst": (1, lambda scope, seq: seq[1:]),
+	"butlast": (1, lambda scope, seq: seq[:-1]),
+	"count": (1, lambda scope, seq: len(seq)),
+	"iseq": (2, lambda scope, a, b: iseq(a, b)),
+	
+	"rnd": (0, lambda scope: random.random()),
+	"random": (2, lambda scope, a, b: random.randint(a, b)),
+	"rerandom": (1, lambda scope, n: random.seed(n)),
+	
+	"timer": (0, lambda scope: time.process_time())
+}
+
+if __name__ == "__main__":
+	import sys
+	
+	toplevel = Scope()
+	#print(parse(sys.argv[1:]))
+	print(run(parse(sys.argv[1:]), toplevel))
