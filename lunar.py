2f4a1046685bd529cc602291b02695b8dca25379
Felix <felix@plesoianu.ro>
2017-01-26 21:16:10 +0200

    Add increment and decrement operators.

--- lunar.py
+++ lunar.py
@@ -248,6 +248,14 @@ def local(varname, scope):
 	else:
 		scope.names[varname.lower()] = None
 
+def plus_one(varname, scope):
+	"""Increment given variable."""
+	scope[varname.lower()] += 1
+
+def minus_one(varname, scope):
+	"""Decrement given variable."""
+	scope[varname.lower()] -= 1
+
 # Conditionals.
 def do_if(cond, code, scope):
 	if cond: return run(code, scope)
@@ -398,6 +406,8 @@ procedures = {
 	"local": (1, lambda scope, n: local(n, scope)),
 	"localmake": (2, lambda scope, a, b: localmake(a, b, scope)),
 	"thing": (1, lambda scope, n: scope[n.lower()]),
+	"+1": (1, lambda scope, n: plus_one(n, scope)),
+	"-1": (1, lambda scope, n: minus_one(n, scope)),
 	
 	"if": (2, lambda scope, a, b: do_if(a, b, scope)),
 	"ifelse": (3, lambda scope, a, b, c: do_ifelse(a, b, c, scope)),
