0592ff59a3a74f65524934d6801229e2d964d44d
Felix <felix@plesoianu.ro>
2017-01-29 10:33:06 +0200

    Fix benchmarks to eliminate I/O variance.

--- benchmarks/benchmark1.go
+++ benchmarks/benchmark1.go
@@ -6,10 +6,10 @@ import "time"
 func main() {
 	start := float64(time.Now().UnixNano()) / (1000 * 1000 * 1000)
 	var a float64 = 1
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 10000; i++ {
 		a = a / 2.0 + a / 3.0
 	}
-	fmt.Println(a)
 	finish := float64(time.Now().UnixNano()) / (1000 * 1000 * 1000)
+	fmt.Println(a)
 	fmt.Println(finish - start)
 }
